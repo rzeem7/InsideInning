c498c3af0b3a811793b33e160fabfd8ac9d52e12
iGagan <gagan.deep91@yahoo.com>
2015-02-18 16:21:39 +0530

    Upload image through API from Web

---
+++ InsideInning.Web.Core/Custom/InsideInning.BO/EmployeeProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsideInning.Web.Core.Custom.InsideInning.BO
+{
+    public class EmployeeProfile
+    {
+        /// <summary>
+        ///  Used to set the File Data and File Name For Image
+        /// </summary>
+        #region Public Properties
+        public string FileName { get; set; }
+        public string FileData { get; set; }
+            
+        #endregion
+       
+    }
+}
--- InsideInning.Web/EmployeeDetails.aspx.cs
+++ InsideInning.Web/EmployeeDetails.aspx.cs
@@ -2,10 +2,13 @@
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using InsideInning.Web.Core.Custom.InsideInning.BO;
 
 namespace InsideInning.Web
 {
@@ -19,26 +22,45 @@ namespace InsideInning.Web
         protected void BtnSave_Click(object sender, EventArgs e)
         {
 
-            var restClient = new RestClient("http://localhost:26197/api/");
-            var req = new RestRequest("EmployeeDetails/", Method.POST);
+            //var restClient = new RestClient("http://localhost:26197/api/");
+            //var req = new RestRequest("EmployeeDetails/", Method.POST);
+
+            var restClient1 = new RestClient("http://localhost:26197/api/");
+            var req1 = new RestRequest("ImageUpload/", Method.POST);
 
 
-            var empDeatilsData = new BOEmployeeDetails();
-            empDeatilsData.gender = RadioBtnFeMale.Checked ? "M" : "F";
-            empDeatilsData.MaritalStatus = RadioBtnMarried.Checked ? "M": "S";
-            empDeatilsData.DateOfBirth = DOBCal.SelectedDate;
-            empDeatilsData.DateOfAniversary = DOACal.SelectedDate;
-            empDeatilsData.ContactNumber = Convert.ToInt32(ContactTxtbox.Text);
-            empDeatilsData.Landline = Convert.ToInt32(lanlinetxtbox.Text);
-            //empDeatilsData.CompanyProfile = CpyProfiletxtbox.Text;
-            empDeatilsData.JoinningDate = JoinCal.SelectedDate;
+            #region Converting the Image To Base64
+
+            byte[] imageArray = System.IO.File.ReadAllBytes("E:/images.jpeg");
+            string base64ImageRepresentation = Convert.ToBase64String(imageArray);
+
+            #endregion
+
+            #region Creating Employee Profile Data And Sending through API
+
+            var _employeeprofile = new EmployeeProfile();
+            _employeeprofile.FileData = base64ImageRepresentation;
+            _employeeprofile.FileName = Guid.NewGuid().ToString() + ".jpeg";
+
+
+            // var empDeatilsData = new BOEmployeeDetails();
+
+            //empDeatilsData.gender = RadioBtnFeMale.Checked ? "M" : "F";
+            //empDeatilsData.MaritalStatus = RadioBtnMarried.Checked ? "M": "S";
+            //empDeatilsData.DateOfBirth = DOBCal.SelectedDate;
+            //empDeatilsData.DateOfAniversary = DOACal.SelectedDate;
+            //empDeatilsData.ContactNumber = Convert.ToInt32(ContactTxtbox.Text);
+            //empDeatilsData.Landline = Convert.ToInt32(lanlinetxtbox.Text);
+
+            //empDeatilsData.JoinningDate = JoinCal.SelectedDate;
             //empDeatilsData.IsActive = CheckIsActive.Checked;
-            
-            req.RequestFormat = RestSharp.DataFormat.Json;
-            req.AddHeader("X-ApiKey", "XMLHttpRequest");
-            req.AddBody(empDeatilsData);
-            restClient.Execute<BOEmployeeDetails>(req);
-        }
 
+            req1.RequestFormat = RestSharp.DataFormat.Json;
+            req1.AddHeader("X-ApiKey", "XMLHttpRequest");
+            req1.AddBody(_employeeprofile);
+            restClient1.Execute<EmployeeProfile>(req1);
+
+            #endregion
+        }
     }
 }

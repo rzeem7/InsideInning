5573daafdc5f6efbe7d4e119b931abcfbe3aba88
rzeem7 <IAm7MOM>
2015-02-17 20:04:11 -0800

    * InsideInning.Web.csproj: * Info.plist: * InsideInning.shproj: * InsideInning.Web.API.csproj: * LoginPageView.cs: * InsideInning.Web.Core.csproj: * InsideInning.iOS.csproj: * InsideInning.Droid.csproj: * MobileBootstrap.cs: * iiTextBoxRenderer.cs: * InsideInning.WinPhone.csproj: * blur.png: * name.png: * Icon.png: * cover.png: * image.png: * email.png: * scott.png: * Notes.png: * Gender.png: * ToDate.png: * leaves.png: * notify.png: * NotePad.png: * Default.png: * Icon-76.png: * persons.png: * twitter.png: * summary.png: * Birthday.png: * Joinning.png: * FromDate.png: * Calendar.png: * password.png: * facebook.png: * slideout.png: * Usrename.png: * instagram.png: * googleplus.png: * Icon-76@2x.png: * Icon-Small.png: * Icon-60@3x.png: * Icon-60@2x.png: * Background.png: * Default@2x.png: * SelectedMale.png: * MaritalStatus.png: * Icon-Small-40.png: * Icon-Small@3x.png: * Icon-Small@2x.png: * ProfilePicture.png: * SelectedFemale.png: * UnselectedMale.png: * SelectedButton.png: * InsideInningiOS.pdb: * InsideInningiOS.exe: * InsideInningiOS.exe: * Default-568h@2x.png: * Default-Portrait.png: * Icon-Small-40@2x.png: * Icon-Small-40@3x.png: * UnselectedButton.png: * UnselectedFemale.png: * Default-Portrait@2x.png: * InsideInningiOS.exe.mdb: * today.png: * shadow.png: * topbar.png: * back.jpg: * blur.png: * datecell.png: * name.png: * Icon.png: * email.png: * cover.png: * Notes.png: * index.jpg: * leftarrow.png: * scott.png: * image.png: * leaves.png: * ToDate.png: * rightarrow.png: * notify.png: * Gender.png: * Info.plist: * persons.png: * NotePad.png: * twitter.png: * Default.png: * summary.png: * Icon-76.png: * Joinning.png: * facebook.png: * slideout.png: * Usrename.png: * Calendar.png: * FromDate.png: * password.png: * Birthday.png: * todayselected.png: * iTunesArtwork: * instagram.png: * Default@2x.png: * Icon-60@2x.png: * Icon-60@3x.png: * Icon-76@2x.png: * Icon-Small.png: * googleplus.png: * Background.png: * datecellmarked.png: * LaunchScreen.plist: * SelectedMale.png: * iTunesArtwork@2x: * datecellselected.png: * Icon-Small-40.png: * Icon-Small@2x.png: * MaritalStatus.png: * Icon-Small@3x.png: * ProfilePicture.png: * UnselectedMale.png: * SelectedButton.png: * SelectedFemale.png: * Default-568h@2x.png: * iTunesMetadata.plist: * UnselectedFemale.png: * UnselectedButton.png: * Default-Portrait.png: * Icon-Small-40@3x.png: * Icon-Small-40@2x.png: * Info.plist: * Default-Portrait@2x.png: * InsideInning.iOS.csproj.FileListAbsolute.txt: * today.png: * topbar.png: * DesignTimeResolveAssemblyReferencesInput.cache: * shadow.png: * Info-8.0+.plist: * datecell.png: * leftarrow.png: * rightarrow.png: * InsideInning.iOS.csprojResolveAssemblyReference.cache: * todayselected.png: * datecellmarked.png: * Info.plist: * datecellselected.png: * Info-8.0+.plist: * TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs: * TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs: * TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs: * objects.nib: * runtime.nib: * runtime.nib: * objects.nib: * objects-8.0+.nib: * objects-8.0+.nib: * objects.nib: * runtime.nib: * runtime.nib: * objects.nib: * objects-8.0+.nib: * objects-8.0+.nib:
    * InsideInning.sln: Improve iOS project and run

--- InsideInning/InsideInning/Pages/LoginPageView.cs
+++ InsideInning/InsideInning/Pages/LoginPageView.cs
@@ -9,85 +9,89 @@ using Color = InsideInning.Helper.Color;
 
 namespace InsideInning.Pages
 {
-    public class LoginPageView : BaseViewPage
-    {
+	public class LoginPageView : BaseViewPage
+	{
 
-        public LoginViewModel ViewModel
-        {
-            get
-            {
-                return BindingContext as LoginViewModel;
-            }
-        }
+		public LoginViewModel ViewModel
+		{
+			get
+			{
+				return BindingContext as LoginViewModel;
+			}
+		}
 
-        public LoginPageView()
-        {
-            BindingContext = new LoginViewModel(this.Navigation);
-            BackgroundImage = "back";
-            Content = new StackLayout
-            {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-               // Padding = new Thickness(100, 100, 100, 100),
-                Children = 
-                {      
-                    CreateStackfor(),
-                }
-            };
-        }
-        private StackLayout CreateStackfor()
-        {
-            var layout = new StackLayout
-            {
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                Padding = new Thickness(30, 30, 30, 30),
-                Spacing = 10,
-                BackgroundColor = Color.Green.ToFormsColor(),
-                Children = 
-                {
-                        CreateEntryFor("Username",Color.iiTextColor,"5"),
-                        CreateEntryFor("Password",Color.iiTextColor, "6", true),
-                        CreateButtonFor("Login"),                             
-                }
-            };
+		public LoginPageView()
+		{
+			BindingContext = new LoginViewModel(this.Navigation);
+			//BackgroundImage = "back.jpg";
+			Content = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				// Padding = new Thickness(100, 100, 100, 100),
+				Children =
+				{      
+					CreateStackfor(),
+				}
+			};
+		}
 
-            return layout;
-        }
-        public static View CreateEntryFor(string propertyName, Color color, string id = "", bool IsPassword = false)
-        {
-            iiTextBox iiEditTextBox = new iiTextBox
-            {
-                TextColor = color.ToFormsColor(),
-                IsPassword = IsPassword,
-                Placeholder = propertyName,
-                BackgroundColor = Xamarin.Forms.Color.Transparent, //Color.iiEditTextColor.ToFormsColor(),
-                ClassId = id,
-                TranslationY = 2,
-                WidthRequest=200,
-            };
-            return iiEditTextBox;
-        }
-        public Button CreateButtonFor(string propertyName)
-        {
-            Button iiButton = new Button
-            {
-                Text = propertyName,
-                TextColor = Color.White.ToFormsColor(),
-                BackgroundColor = Color.Lime.ToFormsColor(),
-                BorderWidth = 1,
-                HorizontalOptions = LayoutOptions.FillAndExpand,
-                HeightRequest=40,
-                FontSize=16,
-                Command = ViewModel.LoginCommand,
-            };
+		private StackLayout CreateStackfor()
+		{
+			var layout = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Padding = new Thickness(30, 30, 30, 30),
+				Spacing = 10,
+				BackgroundColor = Color.Green.ToFormsColor(),
+				Children =
+				{
+					CreateEntryFor("Username", Color.iiTextColor, "5"),
+					CreateEntryFor("Password", Color.iiTextColor, "6", true),
+					CreateButtonFor("Login"),                             
+				}
+			};
 
-            return iiButton;
-        }
-        void iiButton_Clicked(object sender, EventArgs e)
-        {
-            this.Navigation.PushAsync(new DashboardViewPage());
-        }
-    }
+			return layout;
+		}
+
+		public static View CreateEntryFor(string propertyName, Color color, string id = "", bool IsPassword = false)
+		{
+			iiTextBox iiEditTextBox = new iiTextBox
+			{
+				TextColor = color.ToFormsColor(),
+				IsPassword = IsPassword,
+				Placeholder = propertyName,
+				BackgroundColor = Xamarin.Forms.Color.Transparent, //Color.iiEditTextColor.ToFormsColor(),
+				ClassId = id,
+				TranslationY = 2,
+				WidthRequest = 200,
+			};
+			return iiEditTextBox;
+		}
+
+		public Button CreateButtonFor(string propertyName)
+		{
+			Button iiButton = new Button
+			{
+				Text = propertyName,
+				TextColor = Color.White.ToFormsColor(),
+				BackgroundColor = Color.Lime.ToFormsColor(),
+				BorderWidth = 1,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 40,
+				FontSize = 16,
+				Command = ViewModel.LoginCommand,
+			};
+
+			return iiButton;
+		}
+
+		void iiButton_Clicked(object sender, EventArgs e)
+		{
+			this.Navigation.PushAsync(new DashboardViewPage());
+		}
+	}
 }
 
